5d17ab1e3ea70c7e83805e66ef1b9dc1bbc3f957
tvchung <hna.tvchung@gmail.com>
2025-04-28 09:15:11 +0700

    Lesson02 Controller

---
+++ TvcLesson02/Controllers/TvcProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TvcLesson02.Controllers
+{
+    public class TvcProductController : Controller
+    {
+        public IActionResult TvcIndex()
+        {
+            ViewData["messageData"] = "Dữ liệu từ viewData";
+            ViewBag.messageData = "Dữ liệu từ ViewBag";
+            TempData["messageData"] = "Dữ liệu từ TempData";
+            return View();
+        }
+    }
+}
